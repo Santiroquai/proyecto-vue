822d791fc8740970f3a74e3559ef6c37c79903fe
mi primer commit: instalacion de vue 3 <>
2025-04-08 12:04:35 -0300

    Crea componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="decrementar">Decrementar</button>
+  <button @click="incrementar">Incrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++
+}
+const decrementar = () => {
+  contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
